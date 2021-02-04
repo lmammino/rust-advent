e98aaf3885e1b077c171d3509cc03115a204c51e
Luciano Mammino <lucianomammino@gmail.com>
2021-02-04 11:28:57 +0000

    scaffolding of project structure

--- y2020/ex01/src/main.rs
+++ y2020/ex01/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let args = env::args().collect::<Vec<String>>();
+    let input_file_path = args.get(2).expect("Missing input file path argument");
+    let default_part = String::from("part1");
+    let part = args.get(3).unwrap_or(&default_part);
+
+    let input_file = File::open(input_file_path).expect("Cannot open input file");
+    let lines = io::BufReader::new(input_file).lines();
+    let values: Vec<u32> = lines
+        .map(|l| l.expect("Cannot read line from file"))
+        .map(|l| {
+            l.parse::<u32>()
+                .expect(&format!("Cannot convert line '{:?}' to u32", l))
+        })
+        .collect();
+
+    let result = match part.as_str() {
+        "part1" => part1(values),
+        "part2" => part2(values),
+        _ => panic!("Invalid part"),
+    };
+
+    println!("{:?}", result);
+
+    Ok(())
+}
+
+fn part1(values: Vec<u32>) -> u32 {
+    0
+}
+
+fn part2(values: Vec<u32>) -> u32 {
+    0
 }
