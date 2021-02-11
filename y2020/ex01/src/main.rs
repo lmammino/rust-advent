c1b8418a319c9c00c7f6c56d0e2d8c0288543768
Luciano Mammino <lucianomammino@gmail.com>
2021-02-11 11:29:39 +0000

    solved p1 and p2 for 2020/1

--- y2020/ex01/src/main.rs
+++ y2020/ex01/src/main.rs
@@ -1,12 +1,11 @@
-use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::{collections::HashSet, env};
 
 fn main() -> io::Result<()> {
     let args = env::args().collect::<Vec<String>>();
     let input_file_path = args.get(2).expect("Missing input file path argument");
-    let default_part = String::from("part1");
-    let part = args.get(3).unwrap_or(&default_part);
+    let part = args.get(3).unwrap_or(&String::from("part1")).clone();
 
     let input_file = File::open(input_file_path).expect("Cannot open input file");
     let lines = io::BufReader::new(input_file).lines();
@@ -14,25 +13,42 @@ fn main() -> io::Result<()> {
         .map(|l| l.expect("Cannot read line from file"))
         .map(|l| {
             l.parse::<u32>()
-                .expect(&format!("Cannot convert line '{:?}' to u32", l))
+                .unwrap_or_else(|_| panic!("Cannot convert line '{:?}' to u32", l))
         })
         .collect();
 
-    let result = match part.as_str() {
-        "part1" => part1(values),
-        "part2" => part2(values),
+    match part.as_str() {
+        "part1" => assert!(part1(values) == 866436),
+        "part2" => assert!(part2(values) == 276650720),
         _ => panic!("Invalid part"),
     };
-
-    println!("{:?}", result);
 
     Ok(())
 }
 
 fn part1(values: Vec<u32>) -> u32 {
-    0
+    let idx: HashSet<u32> = values.iter().cloned().collect();
+    for v in values {
+        let complement = 2020 - v;
+        if idx.contains(&complement) {
+            return v * complement;
+        }
+    }
+    panic!("Solution not found");
 }
 
 fn part2(values: Vec<u32>) -> u32 {
-    0
+    let idx: HashSet<u32> = values.iter().cloned().collect();
+    for (i, v) in values.iter().enumerate() {
+        for k in values.iter().skip(i) {
+            if v + k > 2020 {
+                continue;
+            }
+            let complement = 2020 - (v + k);
+            if idx.contains(&complement) {
+                return complement * v * k;
+            }
+        }
+    }
+    panic!("Solution not found");
 }
