999bf94d68d8d1001a9995b0c10fe6ec918c7bf7
Luciano <lucianomammino@gmail.com>
2021-03-16 08:31:08 +0000

    Added scaffold for exercise y2020/ex03

---
+++ y2020/ex03/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn part1() -> u32 {
+    // TODO: implement
+    299
+}
+
+pub fn part2() -> u32 {
+    // TODO: implement
+    3621285278
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part_1() {
+        assert_eq!(part1(), 299);
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(part2(), 3621285278);
+    }
+}
