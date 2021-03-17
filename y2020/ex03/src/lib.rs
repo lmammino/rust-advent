b9063df68c315cb55904cb006883bdeb823fc03f
Roberto Gambuzzi <roberto.gambuzzi@fabfitfun.com>
2021-03-17 19:24:30 +0000

    add ex03 test part2

--- y2020/ex03/src/lib.rs
+++ y2020/ex03/src/lib.rs
@@ -32,11 +32,11 @@ pub fn part2(_input: &str) -> u32 {
     let ixs = vec![1,3,5,7,1];
     let mut cnt = vec![0;5];
     for (j, row) in lines.iter().enumerate() {
-        for (i, x) in xs.into_iter().enumerate() {
-            if i==4 && j%2==1{
+        for i in 0..xs.len() {
+            if i==4 && j%2==1 {  // jump the odd row when is the last rule (y must increment by 2)
                 continue
             }
-            let c = row.get(x % row.len()).unwrap();
+            let c = row.get(xs[i] % row.len()).unwrap();
             if *c == '#' {
                 cnt[i] += 1;
             }
