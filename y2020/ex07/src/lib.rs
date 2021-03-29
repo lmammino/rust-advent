cc8ae2813faaab82795095ddd5b89cc6beea2083
Luciano <lucianomammino@gmail.com>
2021-03-29 20:03:23 +0100

    mapped out a potential idea for implementing part 1 of y2020/ex07

--- y2020/ex07/src/lib.rs
+++ y2020/ex07/src/lib.rs
@@ -1,6 +1,46 @@
+// use std::collections::{HashMap, HashSet};
+
+// struct Visitor {
+//     counter: usize,
+//     visited: HashSet<String>,
+//     parents_index: HashMap<&str, Vec<&str>>,
+// }
+
+// impl Visitor {
+// fn new(parents_index: HashMap<&str, Vec<&str>>) -> Self {
+//     Visitor {
+//         counter: 0,
+//         visited: HashSet::new(),
+//         parents_index,
+//     }
+// }
+
+//     fn visit(&mut self, parents_index: HashMap<&str, Vec<&str>>, node: String) -> usize {
+//         self.counter += 1;
+
+//         if self.counter < 10 {
+//             self.visit(parents_index);
+//         }
+
+// TODO: implement. For every parent visit it. If it is not visited, counter + 1 and go on
+
+//         self.counter
+//     }
+// }
+
 pub fn part1(input: &str) -> usize {
+    // let mut parents_index: HashMap<&str, Vec<&str>> = HashMap::new();
+    // // ...
+
+    // let mut counter = 0;
+    // let mut visited: HashSet<&str> = HashSet::new();
+
+    // IDEA creates an instance of visitor and use the visit method on it
+
     println!("{}", input);
     289
+
+    // visitor.visit(parents_index, "first node")
 }
 
 pub fn part2(input: &str) -> usize {
