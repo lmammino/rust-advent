feb0e317d23283d590cb066c45675bef84e19249
Luciano <lucianomammino@gmail.com>
2021-04-15 08:56:42 +0100

    make things green again

--- y2020/ex07/src/lib.rs
+++ y2020/ex07/src/lib.rs
@@ -11,40 +11,42 @@ struct Line {
 type Parents = HashMap<String, Vec<String>>;
 type Children = HashMap<String, Vec<(usize, String)>>;
 lazy_static! {
-    static ref LINE_REGEX: Regex = Regex::new(r"^(\w+\s\w+) bags contain ((?:(?:\d+) (?:\w+\s\w+) bags?(?:[,.]\s?))+|no other bags.)$").unwrap();
+    static ref LINE_REGEX: Regex = Regex::new(
+        r"^(\w+\s\w+) bags contain ((?:(?:\d+) (?:\w+\s\w+) bags?(?:[,.]\s?))+|no other bags.)$"
+    )
+    .unwrap();
     static ref BAGS_REGEX: Regex = Regex::new(r"(\d+) (\w+\s\w+) bags?").unwrap();
 }
 
 fn parse_line(line: &str) -> Line {
-
     let capture = LINE_REGEX.captures(line).unwrap();
     // println!("{:?}", capture);
 
     let name = String::from(&capture[1]);
     let bags = String::from(&capture[2]);
 
-    let mut children: Vec<(usize, String)> = vec!();
+    let mut children: Vec<(usize, String)> = vec![];
     for captured_bags in BAGS_REGEX.captures_iter(&bags) {
         // println!("{:?}", captured_bags);
-        children.push((captured_bags[1].parse().unwrap(), String::from(&captured_bags[2])));
+        children.push((
+            captured_bags[1].parse().unwrap(),
+            String::from(&captured_bags[2]),
+        ));
     }
-    Line {
-        name,
-        children
-    }
+    Line { name, children }
 }
 
-
 pub fn part1(input: &str) -> usize {
-    let bags:Vec<Line> = input.lines().map(parse_line).collect();
+    let bags: Vec<Line> = input.lines().map(parse_line).collect();
 
-    let mut parents : Parents = HashMap::new();
+    let mut parents: Parents = HashMap::new();
 
     for bag in bags.iter() {
         for (_, child) in &bag.children {
-            parents.entry(String::from(child)).or_default().push(
-                String::from(&(bag.name))
-            );
+            parents
+                .entry(String::from(child))
+                .or_default()
+                .push(String::from(&(bag.name)));
         }
     }
 
@@ -65,37 +67,37 @@ pub fn part1(input: &str) -> usize {
                 }
             }
         }
-    };  
+    }
 
     // parents
-    // {"bright white": ["light red", "dark orange"], 
+    // {"bright white": ["light red", "dark orange"],
     // "vibrant plum": ["shiny gold"],
     // "muted yellow": ["light red", "dark orange"],
     // "dotted black": ["dark olive", "vibrant plum"],
     // "faded blue": ["muted yellow", "dark olive", "vibrant plum"],
-    // "shiny gold": ["bright white", "muted yellow"], 
+    // "shiny gold": ["bright white", "muted yellow"],
     // "dark olive": ["shiny gold"]}
 
     visited.len() - 1
 }
 
-paths = ['1type/2type/3other/5type', '1type/2type/3type', '1type/7type/3other/9type']
-
-
 pub fn part2(input: &str) -> usize {
-    let bags:Vec<Line> = input.lines().map(parse_line).collect();
+    let bags: Vec<Line> = input.lines().map(parse_line).collect();
 
-    let mut children : Children = HashMap::new();
+    let mut children: Children = HashMap::new();
 
     for bag in bags.iter() {
         for (count, child) in &bag.children {
-            children.entry(String::from(&(bag.name))).or_default().push(
-                (*count, String::from(child))
-            );
+            children
+                .entry(String::from(&(bag.name)))
+                .or_default()
+                .push((*count, String::from(child)));
         }
     }
 
     println!("{:?}", &children);
+
+    // TODO: continue from here and implement algorithm from part2.png
 
     30055
 }
