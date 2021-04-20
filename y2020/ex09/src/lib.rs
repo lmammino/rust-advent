68fc5a38b7e6fb9a0e26309f5fa26c2b83c679fb
Luciano <lucianomammino@gmail.com>
2021-04-20 09:17:11 +0100

    Added y2020/ex09

---
+++ y2020/ex09/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn part1(input: &str) -> u32 {
+    dbg!(input);
+    0
+}
+
+pub fn part2(input: &str) -> u32 {
+    dbg!(input);
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part_1() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 0);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 0);
+    }
+}
