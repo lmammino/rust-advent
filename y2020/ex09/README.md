8b5ff983a140d4079d050ea0896457e11e6e3fac
Roberto Gambuzzi <roberto.gambuzzi@fabfitfun.com>
2021-04-26 18:48:18 +0100

    add ex09 part1

--- y2020/ex09/README.md
+++ y2020/ex09/README.md
@@ -49,3 +49,42 @@ Here is a larger example which only considers the previous 5 numbers (and has a 
 In this example, after the 5-number preamble, almost every number is the sum of two of the previous 5 numbers; the only number that does not follow this rule is `127`.
 
 The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. **What is the first number that does not have this property?**
+
+Your puzzle answer was `1492208709`.
+
+--- Part Two ---
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+
+Again consider the above example:
+
+```plain
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+```
+
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+
+What is the encryption weakness in your XMAS-encrypted list of numbers?
+
+Your puzzle answer was `238243506`.
+
--- y2020/ex09/src/lib.rs
+++ y2020/ex09/src/lib.rs
@@ -1,9 +1,50 @@
-pub fn part1(input: &str) -> u32 {
-    dbg!(input);
+use std::collections::{HashSet, VecDeque};
+
+fn check(value: &u64, set: &HashSet<u64>) -> bool {
+    for a in set {
+        if a>value {
+            continue;
+        }
+        let b = value - a;
+        if set.contains(&b) {
+            return true;
+        }
+    }
+    false
+}
+
+
+pub fn part1(input: &str) -> u64 {
+    let mut values = input
+            .lines()
+            .map(|l| {
+                l.parse::<u64>()
+                    .unwrap_or_else(|_| panic!("Cannot convert line '{:?}' to u64", l))
+            });
+
+    let mut idx: HashSet<u64> = HashSet::new();
+    let mut queue: VecDeque<u64> = VecDeque::new();
+
+    for _ in 0..25 {
+        let next_value = values.next().expect("no enougth data to start");
+        idx.insert(next_value);
+        queue.push_back(next_value);
+    }
+
+    for value in values {
+        if !check(&value, &idx) {
+            return value;
+        }
+        let remove = queue.pop_front().unwrap();
+        queue.push_back(value);
+        idx.remove(&remove);
+        idx.insert(value);
+    }
+
     0
 }
 
-pub fn part2(input: &str) -> u32 {
+pub fn part2(input: &str) -> u64 {
     dbg!(input);
     0
 }
@@ -14,12 +55,12 @@ mod tests {
     #[test]
     fn part_1() {
         let input = include_str!("../input.txt");
-        assert_eq!(part1(input), 0);
+        assert_eq!(part1(input), 1492208709);
     }
 
     #[test]
     fn part_2() {
         let input = include_str!("../input.txt");
-        assert_eq!(part2(input), 0);
+        assert_eq!(part2(input), 238243506);
     }
 }
