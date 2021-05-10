ff5e02080f1f43c175c2144a54171aaea4ee38f8
Luciano <lucianomammino@gmail.com>
2021-05-10 18:39:11 +0100

    Added combinatorial solution for y2020/ex10 part2

--- y2020/ex10/src/lib.rs
+++ y2020/ex10/src/lib.rs
@@ -1,18 +1,20 @@
 use std::collections::HashSet;
 
 pub fn part1_sort(input: &str) -> u32 {
-    let mut numbers = input.lines()
+    let mut numbers = input
+        .lines()
         .map(|l| {
             l.parse::<u32>()
                 .unwrap_or_else(|_| panic!("Cannot convert line '{:?}' to u32", l))
-        }).collect::<Vec<u32>>();
-        
+        })
+        .collect::<Vec<u32>>();
+
     numbers.sort();
 
     let mut ones = 0;
     let mut threes = 1; // initiating this at 1 because there is a jump of 3 at the end according to the exercise
     let mut last = 0;
-    
+
     for number in numbers {
         let diff = number - last;
         if diff == 1 {
@@ -23,28 +25,30 @@ pub fn part1_sort(input: &str) -> u32 {
         }
         last = number;
     }
-    
+
     ones * threes
 }
 
 pub fn part1(input: &str) -> u32 {
-    let numbers = input.lines()
+    let numbers = input
+        .lines()
         .map(|l| {
             l.parse::<u32>()
                 .unwrap_or_else(|_| panic!("Cannot convert line '{:?}' to u32", l))
-        }).collect::<HashSet<u32>>();
+        })
+        .collect::<HashSet<u32>>();
 
     let mut ones = 0;
     let mut threes = 1; // initiating this at 1 because there is a jump of 3 at the end according to the exercise
     let mut current = 0;
-    
+
     loop {
-        if numbers.contains(&(current+1)) {
+        if numbers.contains(&(current + 1)) {
             current += 1;
-            ones += 1; 
-        } else if numbers.contains(&(current+3)) {
+            ones += 1;
+        } else if numbers.contains(&(current + 3)) {
             current += 3;
-            threes += 1; 
+            threes += 1;
         } else {
             break;
         }
@@ -53,8 +57,46 @@ pub fn part1(input: &str) -> u32 {
     ones * threes
 }
 
+fn tribonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        2 => 2,
+        _ => tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3),
+    }
+}
+
 pub fn part2(input: &str) -> u64 {
-    1511207993344
+    let mut numbers = input
+        .lines()
+        .map(|l| {
+            l.parse::<u32>()
+                .unwrap_or_else(|_| panic!("Cannot convert line '{:?}' to u32", l))
+        })
+        .collect::<Vec<u32>>();
+    numbers.sort_unstable();
+    let mut combinations_by_chunk: Vec<u32> = Vec::with_capacity(input.len());
+
+    let mut prev_num = 0; // first num is zero (impl)
+    let mut elements_in_chunk: usize = 0;
+
+    for num in numbers {
+        if num - prev_num > 2 {
+            combinations_by_chunk.push(tribonacci(elements_in_chunk as u32));
+            elements_in_chunk = 0;
+        } else {
+            elements_in_chunk += 1;
+        }
+        prev_num = num;
+    }
+    // deals last implicit value (+3 implied at the end)
+    combinations_by_chunk.push(tribonacci(elements_in_chunk as u32));
+
+    let result: u64 = combinations_by_chunk
+        .iter()
+        .fold(1, |acc: u64, val| acc * (*val as u64));
+
+    result
 }
 
 #[cfg(test)]
