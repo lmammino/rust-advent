bd86d67225d47760d74560a99a22f737c41577ab
Roberto Gambuzzi <roberto.gambuzzi@fabfitfun.com>
2021-06-17 22:22:42 +0100

    add y2021/ex14 skeleton

---
+++ y2020/ex14/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn part1(input: &str) -> u64 {
+    11884151942312
+}
+
+pub fn part2(input: &str) -> u64 {
+    2625449018811
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part_1() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 11884151942312);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 2625449018811);
+    }
+}
