2d600a923e2a96fbf6340e5971708bfd9cb29919
Roberto Gambuzzi <roberto.gambuzzi@fabfitfun.com>
2021-06-21 19:34:46 +0100

    ex 14 part 1 parsing

--- y2020/ex14/src/lib.rs
+++ y2020/ex14/src/lib.rs
@@ -1,4 +1,36 @@
-pub fn part1(_input: &str) -> u64 {
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Instr {
+    Mask(String),
+    Mem(u64, u64),
+}
+
+impl FromStr for Instr {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("mask") {
+            Ok(Instr::Mask(String::from(&s[7..])))
+        } else if s.starts_with("mem") {
+            let addr_value = &mut s[4..].split("] = ");
+            let addr: u64 = addr_value.next().unwrap().parse().unwrap();
+            let value: u64 = addr_value.next().unwrap().parse().unwrap();
+            Ok(Instr::Mem(addr, value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+
+pub fn part1(input: &str) -> u64 {
+    let instr: Vec<Instr> = input.lines().map(str::parse).map(
+        |x| x.unwrap()
+    ).collect();
+
+    println!("{:?}", instr);
+
     11884151942312
 }
 
