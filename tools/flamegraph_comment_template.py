ca964ce52b230aab5b96aea7cccab221789c3e23
Chiara Malagoli <malagoli@gbinside.com>
2021-07-23 21:12:37 +0100

    add flamegraph workflow

--- tools/flamegraph_comment_template.py
+++ tools/flamegraph_comment_template.py
@@ -9,14 +9,14 @@ for filename in sys.argv[3:]:
     summary = summary[: summary.rfind(".")]
     print(
         f"""
-            <details>
-              <summary>{ summary }</summary>                        
-              <p>
-                <a target="_blank" rel="noopener noreferrer" 
-                          href="https://raw.githubusercontent.com/{ github_repository }/{ hash }/{ filename }">
-                      <img src="https://raw.githubusercontent.com/{ github_repository }/{ hash }/{ filename }" 
-                           alt="" style="max-width:100%;" />
-                </a>
-              </p>
-            </details>"""
+<details>
+    <summary>{ summary }</summary>                        
+    <p>
+    <a target="_blank" rel="noopener noreferrer" 
+                href="https://raw.githubusercontent.com/{ github_repository }/{ hash }/{ filename }">
+            <img src="https://raw.githubusercontent.com/{ github_repository }/{ hash }/{ filename }" 
+                alt="" style="max-width:100%;" />
+    </a>
+    </p>
+</details>"""
     )
