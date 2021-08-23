1d659e507944ec29f66b0d1603b8f5c86298cb1d
Eugen Serbanescu <eserbanescu@demonware.net>
2021-08-23 19:36:14 +0100

    y2020 ex17 part 1 WIP

--- y2020/ex17/README.md
+++ y2020/ex17/README.md
@@ -10,7 +10,7 @@ In the initial state of the pocket dimension, almost all cubes start `inactive`.
 
 The energy source then proceeds to boot up by executing six cycles.
 
-Each cube only ever considers its neighbors: any of the 26 other cubes where any of their coordinates differ by at most 1. For example, given the cube at `x=1,y=2,z=3`, its neighbors include the cube at `x=2,y=2,z=2`, the cube at `x=0,y=2,z=3`, and so on.
+Each cube only ever considers its neighbors: any of the `26` other cubes where any of their coordinates differ by at most 1. For example, given the cube at `x=1,y=2,z=3`, its neighbors include the cube at `x=2,y=2,z=2`, the cube at `x=0,y=2,z=3`, and so on.
 
 During a cycle, all cubes simultaneously change their state according to the following rules:
 
@@ -51,6 +51,14 @@ z=0
 #.#
 .##
 .#.
+
+...
+#.#
+.##
+.#.
+
+
+
 
 z=1
 #..
--- y2020/ex17/src/lib.rs
+++ y2020/ex17/src/lib.rs
@@ -1,4 +1,53 @@
-pub fn part1(_input: &str) -> u32 {
+use std::collections::HashSet;
+
+type Point = (i32, i32, i32);
+type Cube = HashSet<Point>;
+
+fn neighbours_of_point(point: Point) -> [Point; 26] {
+    let mut points = [(0, 0, 0); 26];
+    let mut index = 0;
+    for z in -1..=1 {
+        for y in -1..=1 {
+            for x in -1..=1 {
+                if (z, y, x) != (0, 0, 0) {
+                    points[index] = (x + point.0, y + point.1, z + point.2);
+                    index += 1;
+                }
+            }
+        }
+    }
+
+    points
+}
+
+fn next_state(old_cube: Cube) -> Cube {
+    let cube = Cube::new();
+
+    old_cube.into_iter();
+    // TODO: for each point in the cube, get the neighbours & store temporarily
+    // TODO: for each neighbour AND the current point, we need to apply the rules (read README)
+    // TODO: fn to check alive neighbour count
+
+    cube
+}
+
+pub fn part1(input: &str) -> u32 {
+    let mut cube = Cube::new();
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, cell) in line.chars().enumerate() {
+            let new_point = (x as i32, y as i32, 0);
+            if cell == '#' {
+                cube.insert(new_point);
+            }
+        }
+    }
+
+    for _ in 0..6 {
+        cube = next_state(cube);
+    }
+
+    // TODO: calculate total number of alive points (cube length)
     280
 }
 
@@ -20,5 +69,40 @@ mod ex17_tests {
     fn part_2() {
         let input = include_str!("../input.txt");
         assert_eq!(part2(input), 1696);
+    }
+    #[test]
+    fn test_neighbours() {
+        let result = neighbours_of_point((0, 0, 0));
+        assert_eq!(
+            result,
+            [
+                (-1, -1, -1,),
+                (0, -1, -1,),
+                (1, -1, -1,),
+                (-1, 0, -1,),
+                (0, 0, -1,),
+                (1, 0, -1,),
+                (-1, 1, -1,),
+                (0, 1, -1,),
+                (1, 1, -1,),
+                (-1, -1, 0,),
+                (0, -1, 0,),
+                (1, -1, 0,),
+                (-1, 0, 0,),
+                (1, 0, 0,),
+                (-1, 1, 0,),
+                (0, 1, 0,),
+                (1, 1, 0,),
+                (-1, -1, 1,),
+                (0, -1, 1,),
+                (1, -1, 1,),
+                (-1, 0, 1,),
+                (0, 0, 1,),
+                (1, 0, 1,),
+                (-1, 1, 1,),
+                (0, 1, 1,),
+                (1, 1, 1,),
+            ]
+        );
     }
 }
