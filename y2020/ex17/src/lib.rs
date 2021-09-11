f9e4581723e846c05d4a284eabbf36c0bad5dfda
Luciano Mammino <luciano.mammino@fourtheorem.com>
2021-09-11 16:58:44 +0100

    Cleans up and improves ex17 y2020

--- y2020/ex17/src/lib.rs
+++ y2020/ex17/src/lib.rs
@@ -12,27 +12,69 @@ struct Point<const D: usize>([i32; D]);
 #[derive(Debug)]
 struct Universe<const D: usize>(HashSet<Point<D>>);
 
-fn neighbours_of_point<const D: usize>(point: &Point<D>) -> Vec<Point<D>> {
-    let mut points = vec![];
+#[derive(Debug)]
+struct Game<const D: usize> {
+    universe: Universe<D>,
+    relative_neighbours: Vec<[i32; D]>,
+}
 
-    for i in 0..3_i32.pow(D as u32) {
-        let mut relative_coords = [0; D];
-        let mut n = i;
-        for j in 0..D {
-            relative_coords[j] = (n % 3) - 1;
-            n /= 3;
+impl<const D: usize> Game<D> {
+    fn from_input(input: &str) -> Self {
+        let mut data: HashSet<Point<D>> = HashSet::new();
+
+        for (y, line) in input.lines().enumerate() {
+            for (x, cell) in line.chars().enumerate() {
+                let mut point_data = [0; D];
+                point_data[0] = x as i32;
+                point_data[1] = y as i32;
+                let new_point = Point(point_data);
+                if cell == '#' {
+                    data.insert(new_point);
+                }
+            }
+        }
+        let universe = Universe(data);
+
+        let mut relative_neighbours = vec![];
+
+        for i in 0..3_i32.pow(D as u32) {
+            let mut relative_coords = [0; D];
+            let mut n = i;
+            for coord in relative_coords.iter_mut() {
+                *coord = (n % 3) - 1;
+                n /= 3;
+            }
+            relative_neighbours.push(relative_coords);
         }
 
-        if relative_coords != [0; D] {
-            let mut coords = [0; D];
-            for j in 0..D {
-                coords[j] = relative_coords[j] + point.0[j];
-            }
-            points.push(Point(coords));
+        Game {
+            universe,
+            relative_neighbours,
         }
     }
+}
 
-    points
+impl<const D: usize> Iterator for Game<D> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let new_universe = next_state::<D>(&self.relative_neighbours, &self.universe);
+        self.universe = new_universe;
+        Some(self.universe.0.len())
+    }
+}
+
+fn neighbours_of_point<'a, const D: usize>(
+    point: &'a Point<D>,
+    relative_neighbours: &'a Vec<[i32; D]>,
+) -> impl Iterator<Item = Point<D>> + 'a {
+    relative_neighbours.iter().map(move |neighbour| {
+        let mut data = [0; D];
+        for (i, c) in data.iter_mut().enumerate() {
+            *c = point.0[i] + neighbour[i];
+        }
+        Point(data)
+    })
 }
 
 fn neighbours_of_point_3d(point: &Point3D) -> [Point3D; 26] {
@@ -85,9 +127,12 @@ fn count_active_neighbours_4d(point: &Point4D, cube: &Universe4D) -> usize {
         .count()
 }
 
-fn count_active_neighbours<const D: usize>(point: &Point<D>, universe: &Universe<D>) -> usize {
-    neighbours_of_point(point)
-        .iter()
+fn count_active_neighbours<const D: usize>(
+    point: &Point<D>,
+    relative_neighbours: &Vec<[i32; D]>,
+    universe: &Universe<D>,
+) -> usize {
+    neighbours_of_point(point, relative_neighbours)
         .filter(|p| universe.0.contains(p))
         .count()
 }
@@ -130,11 +175,14 @@ fn next_state_4d(old_universe: Universe4D) -> Universe4D {
         .collect()
 }
 
-fn next_state<const D: usize>(old_universe: Universe<D>) -> Universe<D> {
+fn next_state<const D: usize>(
+    relative_neighbours: &Vec<[i32; D]>,
+    old_universe: &Universe<D>,
+) -> Universe<D> {
     let mut points_to_check: HashSet<Point<D>> = HashSet::new();
     for point in old_universe.0.iter() {
         points_to_check.insert(point.clone());
-        for p in neighbours_of_point(point).iter() {
+        for p in neighbours_of_point(point, relative_neighbours) {
             points_to_check.insert(p.clone());
         }
     }
@@ -143,7 +191,8 @@ fn next_state<const D: usize>(old_universe: Universe<D>) -> Universe<D> {
         points_to_check
             .into_iter()
             .filter(|point| {
-                let active_neighbours = count_active_neighbours(point, &old_universe);
+                let active_neighbours =
+                    count_active_neighbours(point, relative_neighbours, &old_universe);
                 let point_is_alive = old_universe.0.contains(point);
                 matches!((point_is_alive, active_neighbours), (_, 3) | (true, 2))
             })
@@ -171,23 +220,8 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part1_gen(input: &str) -> u32 {
-    let mut data: HashSet<Point<3>> = HashSet::new();
-
-    for (y, line) in input.lines().enumerate() {
-        for (x, cell) in line.chars().enumerate() {
-            let new_point = Point([x as i32, y as i32, 0]);
-            if cell == '#' {
-                data.insert(new_point);
-            }
-        }
-    }
-
-    let mut universe = Universe(data);
-    for _ in 0..6 {
-        universe = next_state(universe);
-    }
-
-    universe.0.len() as u32
+    let mut game = Game::<3>::from_input(input);
+    game.nth(5).unwrap() as u32
 }
 
 pub fn part2(input: &str) -> u32 {
@@ -210,23 +244,8 @@ pub fn part2(input: &str) -> u32 {
 }
 
 pub fn part2_gen(input: &str) -> u32 {
-    let mut data: HashSet<Point<4>> = HashSet::new();
-
-    for (y, line) in input.lines().enumerate() {
-        for (x, cell) in line.chars().enumerate() {
-            let new_point = Point([x as i32, y as i32, 0, 0]);
-            if cell == '#' {
-                data.insert(new_point);
-            }
-        }
-    }
-
-    let mut universe = Universe(data);
-    for _ in 0..6 {
-        universe = next_state(universe);
-    }
-
-    universe.0.len() as u32
+    let mut game = Game::<4>::from_input(input);
+    game.nth(5).unwrap() as u32
 }
 
 #[cfg(test)]
