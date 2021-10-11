c398675ba728aa2cc21406182aba081172c2633c
Luciano Mammino <lucianomammino@gmail.com>
2021-10-11 12:16:32 +0200

    Scaffold y2020 ex 20 (#28)

---
+++ y2020/ex20/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn part1(_input: &str) -> u64 {
+    17032646100079
+}
+
+pub fn part2(_input: &str) -> u64 {
+    2006
+}
+
+#[cfg(test)]
+mod ex20_tests {
+    use super::*;
+
+    #[test]
+    fn part_1() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 17032646100079);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 2006);
+    }
+}
