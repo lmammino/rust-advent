d10419cfae6d847058e43ab06591a4887739b700
Luciano Mammino <lucianomammino@gmail.com>
2021-11-01 20:20:35 +0000

    Solution Y2020 ex20 part 2 (#31)
    * Work in progress ex 20 y2020 part 2
    
    * Added more helper methods
    
    * using match rather than an ugly sequence of if/else
    
    * Added more tests and updated the fit tile right funct
    
    * modularised code
    
    * Added missing files
    
    * wip y2020/ex20 part2
    
    * wip y2020/ex20 part2
    
    * wip y2020/ex20 part2 print the image
    
    * Made clippy happy, fmt and added some more tests
    
    * Finished part2!!!!! 🥳
    
    Co-authored-by: Chiara Malagoli <malagoli@gbinside.com>

--- y2020/ex20/src/lib.rs
+++ y2020/ex20/src/lib.rs
@@ -1,74 +1,13 @@
-use std::{collections::{HashMap, HashSet}, convert::TryInto, str::FromStr};
+mod tile;
+mod tiles_index;
 
-#[derive(Debug)]
-struct Tile<const N: usize> {
-    id: u16,
-    cells: [[char; N]; N],
-    borders: [[char; N]; 8],
-}
-
-struct Tiles<const N: usize>(HashMap<u16, Tile<N>>);
-
-impl<const N: usize> Tile<N> {
-    fn new(id: u16, cells: [[char; N]; N]) -> Self {
-        // In the all following lines the assignment means copy (array implements Copy trait)
-        let top = cells[0];
-        let mut rtop = top;
-        rtop.reverse();
-        let bottom = cells[cells.len() - 1];
-        let mut rbottom = bottom;
-        rbottom.reverse();
-        // `[char; N]` doens't implement FromIterator, so `collect` cannot be used to transform a iterator into an array.
-        // Instead, it is possible to convert Vec<char> into a fixed length array of char through `try_into` method of the trait `TryInto`.
-        let left: [char; N] = cells.iter().map(|r| r[0]).collect::<Vec<char>>().try_into().unwrap();
-        let mut rleft = left;
-        rleft.reverse();
-        let right: [char; N] = cells.iter().map(|r| r[r.len() - 1]).collect::<Vec<char>>().try_into().unwrap();
-        let mut rright = right;
-        rright.reverse();
-
-        let borders: [[char; N]; 8] = [top, rtop, bottom, rbottom, left, rleft, right, rright];
-
-        Tile { id, cells, borders }
-    }
-
-    fn is_neighbour_of(&self, tile: &Tile<N>) -> bool {
-        for border in &self.borders {
-            if tile.borders.contains(border) {
-                return true;
-            }
-        }
-
-        false
-    }
-}
-
-impl<const N: usize> FromStr for Tiles<N> {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut tiles: HashMap<u16, Tile<N>> = HashMap::new();
-
-        for raw_tile in s.split("\n\n").take_while(|t| !t.is_empty()) {
-            let mut lines = raw_tile.lines();
-            let raw_id = lines.next().unwrap();
-            let id: u16 = raw_id[5..9].parse().unwrap();
-            let cells:[[char; N]; N] = lines
-                .map(|l| l.chars().collect::<Vec<char>>().try_into().unwrap())
-                .collect::<Vec<[char; N]>>()
-                .try_into().unwrap();
-    
-            let tile = Tile::new(id, cells);
-            tiles.insert(id, tile);
-        }
-
-        Ok(Self(tiles))
-    }
-}
+use std::collections::{HashMap, HashSet, VecDeque};
+use tile::*;
+use tiles_index::*;
 
 pub fn part1(input: &str) -> u64 {
-    let tiles: Tiles<10> = input.parse().unwrap();
-    
+    let tiles: TilesIndex<10> = input.parse().unwrap();
+
     let mut neighbours: HashMap<u16, HashSet<u16>> = HashMap::new();
 
     for (id, tile) in &tiles.0 {
@@ -96,8 +35,172 @@ pub fn part1(input: &str) -> u64 {
     corners
 }
 
-pub fn part2(_input: &str) -> u64 {
-    2006
+pub fn part2(input: &str) -> usize {
+    let tiles: TilesIndex<10> = input.parse().unwrap();
+
+    let mut neighbours: HashMap<u16, HashSet<u16>> = HashMap::new();
+
+    for (id, tile) in &tiles.0 {
+        for (other_id, other_tile) in &tiles.0 {
+            if id != other_id && tile.is_neighbour_of(other_tile) {
+                neighbours.entry(*id).or_default().insert(*other_id);
+                neighbours.entry(*other_id).or_default().insert(*id);
+            }
+        }
+    }
+
+    // Corner tiles will only have 2 neighbours
+    let corners = neighbours
+        .iter()
+        .filter_map(|(key, val)| {
+            if val.len() == 2 {
+                return Some(*key);
+            }
+
+            None
+        })
+        .collect::<Vec<u16>>();
+
+    let edges = neighbours
+        .iter()
+        .filter_map(|(key, val)| {
+            if val.len() == 3 {
+                return Some(*key);
+            }
+
+            None
+        })
+        .collect::<HashSet<u16>>();
+
+    let mut tilemap: [[u16; 12]; 12] = [[0; 12]; 12];
+    // we take an arbitrary corner (the first one) and we place it at 0,0 in the tilemap
+    let first_corner = *corners.get(0).unwrap();
+    tilemap[0][0] = first_corner;
+    // same for the 2 neighbours of the first corner
+    let neighbours_of_first_corner = neighbours
+        .get(&first_corner)
+        .unwrap()
+        .iter()
+        .collect::<Vec<&u16>>();
+    tilemap[0][1] = *neighbours_of_first_corner[0];
+    tilemap[1][0] = *neighbours_of_first_corner[1];
+
+    let mut stack: VecDeque<(u16, usize, usize)> = VecDeque::new();
+    stack.push_back((tilemap[0][1], 0, 1));
+    stack.push_back((tilemap[1][0], 1, 0));
+
+    while !stack.is_empty() {
+        let (tileid, row, col) = stack.pop_front().unwrap();
+
+        let mut local_neighbours = neighbours.get(&tileid).unwrap().clone();
+        // we try to remove the neighbours that we have already placed
+        if col > 0 {
+            local_neighbours.remove(&tilemap[row][col - 1]);
+        }
+        if row > 0 {
+            local_neighbours.remove(&tilemap[row - 1][col]);
+        }
+        if row < 11 && tilemap[row + 1][col] != 0 {
+            local_neighbours.remove(&tilemap[row + 1][col]);
+        }
+        if col < 11 && tilemap[row][col + 1] != 0 {
+            local_neighbours.remove(&tilemap[row][col + 1]);
+        }
+
+        // if we 2 neighbours left we need to decide which one goes right and below
+        if local_neighbours.len() == 2 {
+            for n in local_neighbours {
+                if corners.contains(&n) || edges.contains(&n) {
+                    tilemap[row][col + 1] = n;
+                } else {
+                    tilemap[row + 1][col] = n;
+                }
+            }
+            stack.push_back((tilemap[row][col + 1], row, col + 1));
+            stack.push_back((tilemap[row + 1][col], row + 1, col));
+        } else if local_neighbours.len() == 1 {
+            let n = *local_neighbours.iter().next().unwrap();
+            tilemap[row + 1][col] = n;
+            stack.push_back((n, row + 1, col));
+        }
+    }
+
+    // now that we have a tilemap of relative tiles
+    // we need to make sure that all the tiles are rotated/flipped correctly
+    // to do that we start with the first tile (top/left corner).
+    // For this one we need to find a possible rotation that fits its bottom and right tile.
+
+    let zero_zero = tiles.0.get(&tilemap[0][0]).unwrap(); // top left corner
+    let zero_one = tiles.0.get(&tilemap[0][1]).unwrap(); // right
+    let one_zero = tiles.0.get(&tilemap[1][0]).unwrap(); // bottom
+    let possible_zero_zero: Vec<Tile<10>> = vec![
+        *zero_zero,
+        zero_zero.rotate(),
+        zero_zero.rotate().rotate(),
+        zero_zero.rotate().rotate().rotate(),
+        zero_zero.flip_horiz(),
+        zero_zero.flip_horiz().rotate(),
+        zero_zero.flip_horiz().rotate().rotate(),
+        zero_zero.flip_horiz().rotate().rotate().rotate(),
+    ];
+
+    let zero_zero_oriented_right: Vec<Tile<10>> = possible_zero_zero
+        .into_iter()
+        .filter(|x| fit_tile_right(x, zero_one).is_some() && fit_tile_bottom(x, one_zero).is_some())
+        .collect();
+
+    // We expect 1 possible tiles
+    assert_eq!(zero_zero_oriented_right.len(), 1);
+
+    let mut img: [[Tile<10>; 12]; 12] = [[zero_zero_oriented_right[0]; 12]; 12];
+    for y in 0..12 {
+        if y > 0 {
+            let top = &img[y - 1][0];
+            let bottom = tiles.0.get(&tilemap[y][0]).unwrap();
+            img[y][0] = fit_tile_bottom(top, bottom).unwrap();
+        }
+        for x in 1..12 {
+            let left = &img[y][x - 1];
+            let right = tiles.0.get(&tilemap[y][x]).unwrap();
+            img[y][x] = fit_tile_right(left, right).unwrap();
+        }
+    }
+
+    // change 1..9 into 0..10 to see the image with border
+
+    let mut image = [[' '; 96]; 96];
+
+    for (row_index, row) in img.iter().enumerate() {
+        for inner_row in 1..9 {
+            for (tile_index, tile) in row.iter().enumerate() {
+                for cell_id in 1..9 {
+                    image[row_index * 8 + inner_row - 1][tile_index * 8 + cell_id - 1] =
+                        tile.cells[inner_row][cell_id];
+                }
+            }
+        }
+    }
+
+    let image_tile = Tile::new(1, image);
+    let possible_images: Vec<Tile<96>> = vec![
+        image_tile,
+        image_tile.rotate(),
+        image_tile.rotate().rotate(),
+        image_tile.rotate().rotate().rotate(),
+        image_tile.flip_horiz(),
+        image_tile.flip_horiz().rotate(),
+        image_tile.flip_horiz().rotate().rotate(),
+        image_tile.flip_horiz().rotate().rotate().rotate(),
+    ];
+
+    for i in possible_images {
+        let num_dragons = i.count_dragons();
+        if num_dragons > 0 {
+            return i.count_sharps() - num_dragons * DRAGON.len();
+        }
+    }
+
+    unreachable!("We must find a rotation that contains dragons");
 }
 
 #[cfg(test)]
