bdbf72a61ff6f419b5b4d359e05ff45be3981b91
Luciano Mammino <luciano.mammino@fourtheorem.com>
2021-12-01 09:12:08 +0000

    y2021 ex1 😅

---
+++ y2021/ex01/src/lib.rs
@@ -0,0 +1,53 @@
+use itertools::Itertools;
+
+pub fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|x| x.parse::<usize>().unwrap())
+        .tuple_windows()
+        .filter(|(prev, next)| next > prev)
+        .count()
+}
+
+pub fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|x| x.parse::<usize>().unwrap())
+        .tuple_windows()
+        .map(|(v1, v2, v3)| v1 + v2 + v3)
+        .tuple_windows()
+        .filter(|(prev, next)| next > prev)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_example() {
+        let input = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        assert_eq!(part1(input), 7);
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 1292);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 1262);
+    }
+}
