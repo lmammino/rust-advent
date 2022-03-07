be5cb18de305ef5d3a3bd83baae9da8da246d3c1
Luciano Mammino <luciano.mammino@fourtheorem.com>
2022-03-07 18:26:46 +0000

    Added comments

--- y2021/ex07/src/lib.rs
+++ y2021/ex07/src/lib.rs
@@ -3,8 +3,17 @@ pub fn part1(input: &str) -> i32 {
         .split(',')
         .map(|n| n.parse::<i32>().unwrap())
         .collect();
+
     positions.sort_unstable();
+    
+    // we have 1000 items in the actual input so the real median would be
+    // the average of the 2 middle points.
+    // Here we are approximating and getting only the first one, but this
+    // gives us the right solution.
+    // A more robust approach, could require to calculate the fuel expenditure
+    // for both the 2 median points, and pick the lowest
     let median = *positions.get(positions.len() / 2).unwrap();
+
     positions.iter().map(|x| (x - median).abs()).sum()
 }
 
@@ -19,6 +28,7 @@ pub fn part2(input: &str) -> i32 {
         .split(',')
         .map(|n| n.parse::<i32>().unwrap())
         .collect();
+
     let avg = positions.iter().sum::<i32>() / positions.len() as i32;
     positions.iter().map(|x| sum_n((x - avg).abs())).sum()
 }
