b227489dc0d4d222d083b0df21fb7ba581041c31
Luciano Mammino <luciano.mammino@fourtheorem.com>
2022-03-07 18:39:56 +0000

    Added benchmark with position_nth_unstable

--- y2021/ex07/src/lib.rs
+++ y2021/ex07/src/lib.rs
@@ -5,7 +5,7 @@ pub fn part1(input: &str) -> i32 {
         .collect();
 
     positions.sort_unstable();
-    
+
     // we have 1000 items in the actual input so the real median would be
     // the average of the 2 middle points.
     // Here we are approximating and getting only the first one, but this
@@ -13,6 +13,25 @@ pub fn part1(input: &str) -> i32 {
     // A more robust approach, could require to calculate the fuel expenditure
     // for both the 2 median points, and pick the lowest
     let median = *positions.get(positions.len() / 2).unwrap();
+
+    positions.iter().map(|x| (x - median).abs()).sum()
+}
+
+pub fn part1_select_nth(input: &str) -> i32 {
+    let mut positions: Vec<i32> = input
+        .split(',')
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+
+    let nth = positions.len() / 2;
+    // we have 1000 items in the actual input so the real median would be
+    // the average of the 2 middle points.
+    // Here we are approximating and getting only the first one, but this
+    // gives us the right solution.
+    // A more robust approach, could require to calculate the fuel expenditure
+    // for both the 2 median points, and pick the lowest
+    let (_, median, _) = positions.select_nth_unstable(nth);
+    let median = *median;
 
     positions.iter().map(|x| (x - median).abs()).sum()
 }
@@ -41,6 +60,12 @@ mod tests {
     fn test_part1() {
         let input = include_str!("../input.txt");
         assert_eq!(part1(input), 347011);
+    }
+
+    #[test]
+    fn test_part1_select_nth() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1_select_nth(input), 347011);
     }
 
     #[test]
