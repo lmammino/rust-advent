0987b26581144e5caf35c7887facb7f355627963
Luciano Mammino <lucianomammino@gmail.com>
2022-07-25 17:52:18 +0100

    Scaffolding y2021/ex19

---
+++ y2021/ex19/src/lib.rs
@@ -0,0 +1,18 @@
+pub 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 4235);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part2(input), 4659);
+    }
+}
