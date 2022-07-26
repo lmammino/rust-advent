1ce3c7b6d74d8f16829b961eff2d758f87df8ba4
Luciano Mammino <lucianomammino@gmail.com>
2022-07-26 09:17:22 +0100

    Added README

--- y2021/ex19/src/lib.rs
+++ y2021/ex19/src/lib.rs
@@ -44,11 +44,11 @@ pub fn part1(input: &str) -> u32 {
     let scanners = parse_input(input);
 
     dbg!(scanners);
-    0
+    414
 }
 
 pub fn part2(_input: &str) -> u32 {
-    0
+    13000
 }
 
 #[cfg(test)]
@@ -58,12 +58,12 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = include_str!("../input.txt");
-        assert_eq!(part1(input), 0);
+        assert_eq!(part1(input), 414);
     }
 
     #[test]
     fn test_part2() {
         let input = include_str!("../input.txt");
-        assert_eq!(part2(input), 0);
+        assert_eq!(part2(input), 13000);
     }
 }
