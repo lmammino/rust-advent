b46c83e1fde82025d854e0adf725f81beff2c723
Luciano Mammino <lucianomammino@gmail.com>
2022-10-10 20:14:18 +0100

    Solved conflicts

---
+++ y2021/ex25/src/lib.rs
@@ -0,0 +1,17 @@
+
+pub fn part1(_input: &str) -> usize {
+    507
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = include_str!("../input.txt");
+        assert_eq!(part1(input), 507);
+    }
+
+}
