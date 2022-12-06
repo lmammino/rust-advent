5c546b39aee6c9bdb5251ebef3f312e8db23be76
Luciano Mammino <lucianomammino@gmail.com>
2022-12-06 17:29:33 +0000

    Solution for day 5

--- y2022/ex05/README.md
+++ y2022/ex05/README.md
@@ -71,9 +71,67 @@ The Elves just need to know which crate will end up on top of each stack; in thi
 
 **After the rearrangement procedure completes, what crate ends up on top of each stack?**
 
-Your puzzle answer was `???`.
+Your puzzle answer was `WHTLRMZRC`.
 
 
 ## Part Two
 
-TBD
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+```plain
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+```
+
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+```plain
+[D]        
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+```
+
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+```plain
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+```
+
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+```plain
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+```
+
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+```plain
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+```
+
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, **what crate ends up on top of each stack?**
+
+Your puzzle answer was `GMPMLWNMG`.
--- y2022/ex05/src/lib.rs
+++ y2022/ex05/src/lib.rs
@@ -1,9 +1,76 @@
-pub fn part1(_input: &str) -> u64 {
-    0
+mod parser;
+use parser::*;
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct Move {
+    quantity: usize,
+    from: usize,
+    to: usize,
 }
 
-pub fn part2(_input: &str) -> u64 {
-    0
+#[derive(Debug)]
+pub struct Stacks(Vec<Vec<char>>);
+
+impl Stacks {
+    fn apply_move(&mut self, mov: Move) {
+        for _ in 0..mov.quantity {
+            let item = self.0.get_mut(mov.from - 1).unwrap().pop().unwrap();
+            self.0.get_mut(mov.to - 1).unwrap().push(item);
+        }
+    }
+
+    fn apply_move_9001(&mut self, mov: Move) {
+        let mut popped: Vec<char> = Vec::new();
+        for _ in 0..mov.quantity {
+            let item = self.0.get_mut(mov.from - 1).unwrap().pop().unwrap();
+            popped.push(item);
+        }
+        for item in popped.iter().rev() {
+            self.0.get_mut(mov.to - 1).unwrap().push(*item);
+        }
+    }
+
+    fn get_top_text(&self) -> String {
+        let top_text: String = self.0.iter().map(|s| s.last().unwrap()).collect();
+        top_text
+    }
+}
+
+impl From<Vec<Vec<Option<char>>>> for Stacks {
+    fn from(v: Vec<Vec<Option<char>>>) -> Self {
+        let mut stacks = Vec::new();
+        for _ in 0..v.len() {
+            stacks.push(Vec::new());
+        }
+        for line in v.iter().rev() {
+            for (i, crate_) in line.iter().enumerate() {
+                if let Some(c) = crate_ {
+                    stacks.get_mut(i).unwrap().push(*c);
+                }
+            }
+        }
+        Stacks(stacks)
+    }
+}
+
+pub fn part1(input: &str) -> String {
+    let (_, (mut stacks, moves)) = parse_input(input).unwrap();
+
+    for mov in moves {
+        stacks.apply_move(mov);
+    }
+
+    stacks.get_top_text()
+}
+
+pub fn part2(input: &str) -> String {
+    let (_, (mut stacks, moves)) = parse_input(input).unwrap();
+
+    for mov in moves {
+        stacks.apply_move_9001(mov);
+    }
+
+    stacks.get_top_text()
 }
 
 #[cfg(test)]
@@ -13,11 +80,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(INPUT), 0);
+        assert_eq!(part1(INPUT), "WHTLRMZRC".to_string());
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(INPUT), "GMPMLWNMG".to_string());
     }
 }
